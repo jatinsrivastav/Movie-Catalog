762bc6c094ae8cf52a1303728675073785539cd6
jatin <jatinsrivastav.mrj@gmail.com>
2023-07-30 15:05:10 +0530

    first commit

---
+++ src/main/java/com/movie_catalog/MovieCatalogApplication.java
@@ -0,0 +1,14 @@
+package com.movie_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogApplication.class, args);
+		System.out.println("jj");
+	}
+
+}
